04c8e21c50abf6a58187ee54fe8f0715ab864818
Stephen Brown <steve@evolvedlight.co.uk>
2012-12-04 17:57:42 +0000

    Added basic project setup using setuptools

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='PyDashy',
+    version='0.1dev',
+    packages=['pydashy',],
+    license='MIT',
+    long_description=open('README.txt').read(),
+)
